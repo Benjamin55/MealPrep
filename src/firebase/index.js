7915c38b822620cd5fb7f6e9286d2d407a8dc33c
Benjamin55 <bajramovicbenjamin91@gmail.com>
2020-12-23 15:29:51 +0100

    full meal prep done

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDVgLv034c-c4-h9Ti2lX360D122jf82l0",
+    authDomain: "my-project-1569872842915.firebaseapp.com",
+    databaseURL: "https://my-project-1569872842915.firebaseio.com",
+    projectId: "my-project-1569872842915",
+    storageBucket: "my-project-1569872842915.appspot.com",
+    messagingSenderId: "758048722925",
+    appId: "1:758048722925:web:a03dbc6ebf1baba2c521e4",
+    measurementId: "G-S3YM56BJK9"
+};
+firebase.initializeApp(config);
