af8ab975905f7cc5f73e8c4c2b7755fb21501faf
Benjamin55 <bajramovicbenjamin91@gmail.com>
2020-12-23 15:23:45 +0100

    comiittt

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import firebase from "./firebase/index.js";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  axios,
+  firebase,
   vuetify,
   render: h => h(App)
 }).$mount("#app");
